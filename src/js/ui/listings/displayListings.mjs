51b7064cbe041a9810f0a02a52d0bc396126ba0e
KamlaF <100962390+KamlaF@users.noreply.github.com>
2023-09-18 22:42:51 +0200

    css

--- src/js/ui/listings/displayListings.mjs
+++ src/js/ui/listings/displayListings.mjs
@@ -4,8 +4,8 @@ export function renderListings(listings) {
   let html = "";
   listings.forEach((listing) => {
     html += `
-     <div class="listing-item col-12 col-sm-6 col-md-4 col-lg-3 mb-4 p-2">
-    <div class="card">
+     <div class="listing-item col-12 col-sm-6 col-md-4 col-lg-3 mb-4 p-2 ">
+    <div class="card ">
       <img class="card-img-top" src="${listing.media[0]}" alt="${listing.title}" />
       <div class="card-body">
         <h3 class="card-title">${listing.title}</h3>
