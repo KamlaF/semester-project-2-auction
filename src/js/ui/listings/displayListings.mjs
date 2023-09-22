4db7b1bfd261e4fb274420355cae557679a31d55
KamlaF <100962390+KamlaF@users.noreply.github.com>
2023-09-22 22:58:20 +0200

    l

--- src/js/ui/listings/displayListings.mjs
+++ src/js/ui/listings/displayListings.mjs
@@ -3,6 +3,7 @@ export function renderListings(listings) {
 
   let html = "";
   listings.forEach((listing) => {
+    
     html += `
      <div class="listing-item col-12 col-sm-6 col-md-4 col-lg-3 mb-4 p-2 ">
     <div class="card ">
