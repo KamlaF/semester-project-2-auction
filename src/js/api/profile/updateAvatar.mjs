2b30dd1fc4252d6942b17dc40e5b3c360ce87e44
KamlaF <100962390+KamlaF@users.noreply.github.com>
2023-09-24 21:41:44 +0200

    Avatar ok

--- src/js/api/profile/updateAvatar.mjs
+++ src/js/api/profile/updateAvatar.mjs
@@ -1,36 +1,22 @@
 import { API_UPDATE_AVATAR } from "../../constants/api.mjs";
 import { headers } from "/src/js/api/headers.mjs";
 
-export async function updateAvatar(username, avatarUrl) {
-  console.log("Sending token:", localStorage.getItem("token"));
+export async function updateAvatar(avatarUrl) {
+  const profileString = localStorage.getItem("profile");
+  const profile = profileString ? JSON.parse(profileString) : null;
 
-  const response = await fetch(API_UPDATE_AVATAR(username), {
+  if (!profile || !profile.name) {
+    throw new Error("No profile name found in local storage");
+  }
+
+  const response = await fetch(API_UPDATE_AVATAR(profile.name), {
     method: "PUT",
     body: JSON.stringify({ avatar: avatarUrl }),
     headers: headers("application/json"),
   });
 
   if (response.ok) {
-    const updatedProfile = await response.json();
-
-    // Store the new avatar URL in local storage
-    localStorage.setItem("avatarUrl", updatedProfile.avatar);
-
-    return updatedProfile;
+    return avatarUrl; 
   }
-
   throw new Error(response.statusText);
-}
-
-// Function to retrieve the stored avatar URL
-export function getStoredAvatarUrl() {
-  return localStorage.getItem("avatarUrl");
-}
-
-// Function to set the avatar image source to the stored URL
-export function displayStoredAvatar() {
-  const storedAvatarUrl = getStoredAvatarUrl();
-  if (storedAvatarUrl) {
-    document.getElementById("displayedAvatar").src = storedAvatarUrl;
-  }
 }
